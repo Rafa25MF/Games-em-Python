715e72f3d0065db6460ccf23880f1a431d2be360
Rafael Mistura <rafaelfernandesm12345@gmail.com>
2025-09-14 16:42:04 -0300

    Add files via upload

---
+++ Jogo de Detetive.py
@@ -0,0 +1,300 @@
+import time
+
+                                            
+def medonho():
+    print("\033[32m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣶⠶⠒")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣠⣿⣿⠿⠏⠀⠀⠀⠀⠰⣦⡀")
+    print("⠀⠀⠀⠀⠀⠴⠖⠻⢿⡿⠿⢿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠉⠻⡾⠚⠛⠉⠁")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⢀⣼⣗⣄⠀⠀⠀⢀⠲⣆⠀⠀⠀⠀⠀⠀⠐⢦⣤⡀")
+    print("⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣎⠻⣦⣴⢧⣠⡌⢻⣶⣤⡀⠀⠀⠀⠈⠙⢿⣆⣀⢀⣀⡀")
+    print("⠀⠀⠀⠠⠴⠶⣶⣾⠃⠀⠀⢀⣀⣶⣿⣿⣿⢿⣿⠿⣿⣷⣌⣿⣿⠛⠃⣸⣿⣿⡷⠀⠀⠀⠀⠀⠘⠛⠋⠛⠁")
+    print("⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠈⢉⣩⣿⣿⠋⠣⢀⣇⡤⠚⠙⢿⣿⡝⣿⣦⣿⣿⣿⡀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⠙⢿⣿⣿⣿⣷⣄⠀⠀⠀⢀⣶⣿⣄")
+    print("⠀⠀⠀⠀⠀⠀⠀⣉⠙⠛⠉⢹⣿⣿⣿⠛⠛⠛⣿⡛⠛⠛⠛⣿⣿⣿⢸⣷⣿⣿⣿⣿⣿⡷⠤⠐⢻⡿⢻⣿⠂")
+    print("⠀⠀⠀⠀⠀⠀⢰⣿⣷⣦⠄⠈⣿⣿⣿⠀⠀⠀⣿⡇⣀⣀⣀⣿⣿⣿⢸⣿⣿⠟⢿⣿⣿⣿⠢⠄⢸⣇⣸⡇")
+    print("⠀⠀⠀⠀⠀⠀⠁⠘⢯⢙⣧⠀⠹⣿⣿⡏⠉⠉⢹⡏⠉⠉⠉⣿⣿⡇⣸⣿⡏⠀⠘⣿⣿⠇⠀⠀⢸⣿⠉⠀⠀⢀⣤⣴")
+    print("⠀⠀⠀⣴⣦⠀⠀⠀⠀⠙⣿⣶⣤⣿⣿⣧⠀⠀⢸⡇⠀⠀⢰⣿⣿⡇⣿⣿⣿⣿⣶⣿⣿⢀⣀⣀⣸⣿⣶⣶⣿⣿⣟⣿⣦")
+    print("⠀⠀⢠⣿⣿⡿⣦⣄⡀⠀⠀⠀⠈⢻⣿⣿⡍⠉⠉⡏⠉⠉⣹⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆")
+    print("⢀⣴⣿⣿⣿⣿⣿⢿⣿⣷⣤⣀⡀⠘⣿⣿⣇⣀⣀⣇⣀⣀⣿⣿⣿⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣻⣾⣿⠿⣿⣿⣿⣷⠄")
+    print("⢉⣽⣿⣿⡏⠈⢻⣷⣝⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣛⣛⣋⣉⣡⣾⣿⣿⠃⠀⣿⣿⣿⣿⣦⡀")
+    print("⠛⠋⠁⢻⣿⣤⣼⣿⣿⣿⣷⠾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⣿⠛⢻⣿⣿⣿⣿⣇⣀⣠⣿⣿⡻⢮⡏⠉")
+    print("⠀⠀⠀⠈⢿⣿⠉⠻⣿⣿⣿⣀⣻⣸⣿⣿⣿⡿⠛⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⡟⠀⢸⣿⣿⣿⣿⠟⠉⣿⣿⡯⠋⡆⡇")
+    print("⠀⠀⠀⠀⠘⣿⣦⣤⣿⣿⣿⡇⢸⠈⣿⣿⣿⠃⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⡏⠉⡏⠉⣿⣿⣿⣿⣿⣀⣀⣿⠟⠁⠐⠒⡇")
+    print("⠿⡆⠀⢷⡄⣿⣿⣿⣿⣿⣿⣿⣾⣷⣿⣿⣿⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣧⣴⣁⣀⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⡋⠁⠁")
+    print("⠀⢻⠀⠈⣷⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⢀⡾⠃⠀⣸⠂")
+    print("⠲⠺⡟⠋⢹⡉⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⢈⡿⠙⠳⣶⣯⡀")
+    print("⠤⠴⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠥⠤⠀⠀⠀⠀⠉⢉⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⡿⠛⢷⡾⣇⡀")
+    print("⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⡟⠛⠲⠶⠶⠦⠤⠤⠤⠤⠤⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⡀")
+    print("⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⢿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⠛⠛⠛⠛⠻⠿⠿⠿⠛⠛⠛")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠿⠿⠿⠿⠿⠿⠿⠟⠛⠛⠉⠁\033[m")
+
+def morto():
+ print("\033[31m⠄⠄⠄⣠⣤⢶⣻⣿⣻⣿⣿⣿⣿⣿⣿⣦⣤⣀        ⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣦⣾⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+ print("⠄⠄⣼⣺⢷⣻⣽⣾⣿⢿⣿⣷⣿⣿⢿⣿⣿⣿⣇        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣶⢠⣦⣤⣶⣾⣽⣯⣭⣭⣽⣯⣭⣿⣯⣿⣽⣿⣿⣿⣿⣿⣷⣯⣧⣴⣁⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠀⣀⡀")
+ print("⠠⡍⢾⣺⢽⡳⣻⡺⣽⢝⢗⢯⣻⢽⣻⣿⣿⣿⣿⢿⡄   ==   ⢐⣶⣶⣶⣦⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⡭⣕⣥⣦⣤⣵⣴⣤⡊⣷⣜⣷⢢⣶⡴⠴⢮⣊⢓⣴⣶⣾⣿⣿⡓⣿⡿⢿⢯⣯⣽⡹⠟⠻⠿⢛⠟⡛⢻⣿⣿⣿⣟⡿⢟⣻⠻⣿⣷⣴⢦⠤⢤⡤⢤⣴⣶⣿⡿⣿⣮⢹⠇")
+ print("⡨⣖⢹⠜⢅⢫⢊⢎⠜⢌⠣⢑⠡⣹⡸⣜⣯⣿⢿⣻⣷        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠓⣻⣿⣭⣭⣭⣯⣭⣭⣭⣭⣭⣭⣭⣭⣽⣭⣭⣿⣿⡻⣯⢱⣿⢙⣿⣿⢝⣿⡿⢀⣀⡈⢠⡻⢍⠀⠠⡂⣳⣿⣿⣟⣿⣟⣅⣿⣇⣧⠠⡈⣷⣷⢫⠚⡆⣶⡌⠙⡏⢸⠀")
+ print("⢜⢔⡹⡭⣪⢼⠽⠷⠧⣳⢘⢔⡝⠾⠽⢿⣷⣿⣟⢷⣟        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠉⠘⠓⠒⠗⠒⠒⠒⠚⠛⠒⣻⣿⢿⠵⡒⠍⢎⡉⠓⡖⣇⣿⣗⣾⣚⣿⣛⣿⣾⣿⣿⣾⣷⣶⣶⣿⣿⣾⣿⢿⠋⠛⠛⠛⠛⠛⠚⠞⠚⠓⠒⠯⣰⠸⣧⢀⠃⣿⠀")
+ print("⢸⢘⢼⠿⠟⠁⠄⠄⡀⠄⠃⠑⡌⠄⠄⠈⠙⠿⣷⢽⣻        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠛⠚⠍⠞⠀⠀⠁⠈⣿⣿⣿⣽⣿⠿⣿⣿⠛⠛⣿⣿⣯⣿⣿⠟⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠿⠟⢛⣳⣄⠏⠀")
+ print("⢌⠂⠅⠄⠄⠄⠄⠄⠄⡀⣲⣢⢂⠄⠄⠄⠄⠄⠈⣯⠏        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⣿⣿⣿⣿⡏⠙⠠⠤⠯⠑⢛⣵⣟⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+ print("⠐⠨⡂⠄⠄⠄⠄⠄⡀⡔⠋⢻⣤⡀⠄⠄⢀⠄⢸⣯⠇        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣯⣿⣿⣿⣯⣇⠀⠀⠀⠀⠀⠈⢒⡿⡯⡧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+ print("⠈⣕⠝⠒⠄⠄⠒⢉⠪⠄⠄⠄⢿⠜⠑⠢⠠⡒⡺⣿⠖        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠉⠙⡍⠏⡯⣧⠀⠀⠀⠀⠀⠀⠈⣾⣿⠽⠷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+ print("⠄⠐⠅⠁⡀⠄⠐⢔⠁⠄⠄⠄⢀⢇⢌⠄⠄⠄⠸⠕        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣖⣃⣃⣐⣀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+ print("⠄⠄⠂⠄⠄⠨⣔⡝⠼⡄⠂⣦⡆⣿⣲⠐⠑⠁⠄⠃")
+ print("⠄⠄⠄⠄⠄⠃⢫⢛⣙⡊⣜⣏⡝⣝⠆\033[m")
+ 
+def bizarro():
+ print("⠄⠄⠄⣠⣤⢶⣻⣿⣻⣿⣿⣿⣿⣿⣿⣦⣤⣀")
+ print("⠄⠄⣼⣺⢷⣻⣽⣾⣿⢿⣿⣷⣿⣿⢿⣿⣿⣿⣇")
+ print("⠠⡍⢾⣺⢽⡳⣻⡺⣽⢝⢗⢯⣻⢽⣻⣿⣿⣿⣿⢿⡄")
+ print("⡨⣖⢹⠜⢅⢫⢊⢎⠜⢌⠣⢑⠡⣹⡸⣜⣯⣿⢿⣻⣷")
+ print("⢜⢔⡹⡭⣪⢼⠽⠷⠧⣳⢘⢔⡝⠾⠽⢿⣷⣿⣟⢷⣟")
+ print("⢸⢘⢼⠿⠟⠁⠄⠄⡀⠄⠃⠑⡌⠄⠄⠈⠙⠿⣷⢽⣻")
+ print("⢌⠂⠅⠄⠄⠄⠄⠄⠄⡀⣲⣢⢂⠄⠄⠄⠄⠄⠈⣯⠏")
+ print("⠐⠨⡂⠄⠄⠄⠄⠄⡀⡔⠋⢻⣤⡀⠄⠄⢀⠄⢸⣯⠇")
+ print("⠈⣕⠝⠒⠄⠄⠒⢉⠪⠄⠄⠄⢿⠜⠑⠢⠠⡒⡺⣿⠖")
+ print("⠄⠐⠅⠁⡀⠄⠐⢔⠁⠄⠄⠄⢀⢇⢌⠄⠄⠄⠸⠕")
+ print("⠄⠄⠂⠄⠄⠨⣔⡝⠼⡄⠂⣦⡆⣿⣲⠐⠑⠁⠄⠃")
+ print("⠄⠄⠄⠄⠄⠃⢫⢛⣙⡊⣜⣏⡝⣝⠆")
+
+
+
+print("\nJogo de Detetive Interativo")
+print("Você é um detetive encarregado de resolver um misterioso assassinato que aconteceu em uma mansão.")
+print("Seu objetivo é descobrir:\n")
+print("1. Quem é o culpado?\n"
+"2. Onde ocorreu o crime?\n"
+"3. Qual arma foi utilizada?\n")
+print("Então, que encarar essa missão?")
+print("Com base em suas escolhas durante a investigação, você poderá encontrar pistas, acusar suspeitos, seguir caminhos errados ou até ser eliminado.")
+
+
+decisao = (input("Aceita? (S/N) " )).strip()
+if decisao == "S" or decisao == "s":
+    print("Ótima escolha!")
+    time.sleep(3)
+    print("Vamos às Informações do Caso:")
+    print("\n● Suspeitos:")
+    print("○ Sra. Black")
+    print("○ Coronel Mustard")
+    print("○ Professor Plum")
+    time.sleep(3)
+    print("\n● Locais:")
+    print("○ Biblioteca")
+    print("○ Cozinha")
+    print("○ Sala de estar")
+    time.sleep(3)
+    print("\n● Armas:")
+    print("○ Faca")
+    print("○ Revólver")
+    print("○ Castiçal")
+    
+
+    def arma():
+        print("\nA espingarda está apontada para a esquerda, sem nada ao seu redor.")
+        time.sleep(2)
+        print("A tensão é palpável, e algo estranho está prestes a acontecer...")
+        time.sleep(2)
+        restart()
+
+    def medonho():
+        print("\nVocê entra em uma casa assombrada. O ar está gelado e há sons estranhos ao fundo.")
+        time.sleep(2)
+        print("As sombras parecem ganhar vida. Você sente que está sendo observado.")
+        time.sleep(2)
+        restart()
+
+    def morto():
+        print("\nUma caveira aparece, segurando uma espingarda apontada para a direita.")
+        time.sleep(2)
+        print("Você vê que não há mais escapatória. Sua sorte acabou.")
+        time.sleep(2)
+        restart()
+
+    def bizzaro():
+        print("\nVocê vê uma caveira, e nada mais. O silêncio é absoluto.")
+        time.sleep(2)
+        print("Algo macabro está acontecendo aqui...")
+        time.sleep(2)
+        restart()
+
+    def restart():
+        print("\nVocê quer tentar novamente?")
+        escolha = input("Digite 'sim' para tentar ou 'não' para sair: ")
+
+        if escolha.lower() == "sim":
+            jogar()
+        else:
+            print("\nVocê decidiu abandonar. O mistério permanece... até a próxima.")
+            exit()
+
+    time.sleep(2)
+    print("\nComeçando")
+    time.sleep(5)
+    print("\n─────────────────────────────")
+    print("     MANSÃO MISTERIOSA ")
+    print("─────────────────────────────")       
+
+
+    def jogar():
+        print("Você acorda em uma sala escura. Um cheiro forte de mistério no ar... e um pouco de café queimado.")
+        time.sleep(2)
+        print("Você sente que algo não está certo. Há um corpo no canto e você precisa descobrir o que aconteceu.")
+        time.sleep(2)
+        print("Mas atenção, suas decisões podem ser fatais... Literalmente.")
+        time.sleep(2)
+
+
+        print("\nVocê está em uma mansão misteriosa. O que você faz?")
+        print("1. Examinar o corpo")
+        print("2. Procurar por pistas na sala")
+        print("3. Tentar sair da sala imediatamente (Sério?)")
+
+        escolha = input("Digite o número da sua escolha: ")
+
+        if escolha == "1":
+            print("\nVocê se aproxima do corpo. É um homem de meia-idade, com uma expressão de dor.")
+            time.sleep(2)
+            print("Ao examinar mais de perto, percebe que há um bilhete dobrado em sua mão.")
+            time.sleep(2)
+            print("No bilhete está escrito: 'Cuidado com a faca!'")
+            time.sleep(2)
+            print("Você tem duas opções agora...")
+            print("1. Ignorar o bilhete e continuar investigando a sala")
+            print("2. Levar o bilhete a sério e procurar por uma faca")
+
+            escolha = input("Digite o número da sua escolha: ")
+            if escolha == "1":
+                print("\nVocê decide ignorar o bilhete e procurar pistas na sala.")
+                time.sleep(2)
+                print("Você começa a procurar e encontra uma carta rasgada e uma foto de alguém que você não reconhece.")
+                time.sleep(2)
+                print("De repente, uma voz misteriosa ecoa pela sala: 'Nada do que você encontrar aqui vai te salvar!'")
+                time.sleep(2)
+                print("Agora você decide...")
+                print("1. Ignorar a voz e continuar investigando")
+                print("2. Parar e tentar falar com a voz misteriosa (Ou correr pra porta!)")
+
+                escolha = input("Digite o número da sua escolha: ")
+                if escolha == "1":
+                    print("\nVocê ignorou a voz... Erro fatal! Acontece que a voz estava avisando sobre o grande perigo...")
+                    bizzaro()
+                elif escolha == "2":
+                    print("\nVocê grita: 'Quem está aí?'")
+                    time.sleep(2)
+                    print("A voz responde: 'Você está em uma enrascada, meu amigo. O assassino está mais perto do que imagina.'")
+                    time.sleep(2)
+                    print("De repente, a sala começa a se contorcer. As paredes se aproximam e você sente uma pressão enorme.")
+                    time.sleep(2)
+                    print("Agora, é hora de agir rápido!")
+                    print("1. Correr para a janela e tentar escapar")
+                    print("2. Continuar conversando com a voz")
+
+                    escolha = input("Digite o número da sua escolha: ")
+                    if escolha == "1":
+                        print("\nVocê corre para a janela e, com um esforço brutal, consegue quebrá-la!")
+                        time.sleep(2)
+                        print("Mas antes de saltar, você percebe algo estranho: a voz misteriosa rindo ao fundo...")
+                        time.sleep(2)
+                        print("Você se vira e vê o verdadeiro assassino na sala: alguém que você conhecia!")
+                        time.sleep(2)
+                        print("O assassino sorri, e você percebe que estava sendo manipulado o tempo todo!")
+                        time.sleep(2)
+                        print("\nParabéns, você encontrou a verdade... mas foi tarde demais!")
+                        morto()
+                    elif escolha == "2":
+                        print("\nContinuar conversando com a voz não ajudou. A pressão das paredes começa a esmagar seu corpo!")
+                        arma()
+                    else:
+                        print("\nVocê ficou paralisado. Não sabe se é um pesadelo ou realidade.")
+                        medonho()
+
+            elif escolha == "2":
+                print("\nVocê decide procurar por uma faca que o bilhete mencionava.")
+                time.sleep(2)
+                print("Após procurar por um tempo, você encontra uma faca escondida embaixo de uma prateleira.")
+                time.sleep(2)
+                print("Você tem em suas mãos a chave para se proteger... ou a arma para cometer um erro fatal.")
+                time.sleep(2)
+                print("Agora, você tem uma nova escolha...")
+                print("1. Investigar mais a sala, agora com a faca")
+                print("2. Sair da sala com a faca em mãos")
+
+                escolha = input("Digite o número da sua escolha: ")
+                if escolha == "1":
+                    print("\nVocê decide investigar mais... mas acaba sendo atacado pelo verdadeiro assassino.")
+                    arma()
+                elif escolha == "2":
+                    print("\nVocê corre em direção à porta, mas antes que consiga sair... uma sombra misteriosa aparece atrás de você!")
+                    medonho()
+                else:
+                    print("\nVocê ficou congelado com a faca na mão, olhando para nada e para tudo ao mesmo tempo.")
+                    bizzaro()
+
+            else:
+                print("\nVocê ficou paralisado e a situação ficou ainda mais estranha. O tempo é curto... A sala começa a se fechar.")
+                arma()
+
+        elif escolha == "2":
+            print("\nVocê começa a procurar pistas pela sala. Há uma mesa bagunçada e papéis espalhados.")
+            time.sleep(2)
+            print("Você encontra uma carta rasgada e uma foto de alguém que você não reconhece.")
+            time.sleep(2)
+            print("De repente, uma voz misteriosa ecoa pela sala: 'Nada do que você encontrar aqui vai te salvar!'")
+            time.sleep(2)
+            print("Você decide...")
+            print("1. Ignorar a voz e continuar investigando")
+            print("2. Parar e tentar falar com a voz misteriosa (Ou correr pra porta!)")
+
+            escolha = input("Digite o número da sua escolha: ")
+            if escolha == "1":
+                print("\nVocê ignorou a voz... Erro fatal! Acontece que a voz estava avisando sobre o grande perigo...")
+                bizzaro()
+            elif escolha == "2":
+                print("\nVocê grita: 'Quem está aí?'")
+                time.sleep(2)
+                print("A voz responde: 'Você está em uma enrascada, meu amigo. O assassino está mais perto do que imagina.'")
+                time.sleep(2)
+                print("De repente, a sala começa a se contorcer. As paredes se aproximam e você sente uma pressão enorme.")
+                time.sleep(2)
+                print("Agora, é hora de agir rápido!")
+                print("1. Correr para a janela e tentar escapar")
+                print("2. Continuar conversando com a voz")
+
+                escolha = input("Digite o número da sua escolha: ")
+                if escolha == "1":
+                    print("\nVocê corre para a janela e, com um esforço brutal, consegue quebrá-la!")
+                    time.sleep(2)
+                    print("Mas antes de saltar, você percebe algo estranho: a voz misteriosa rindo ao fundo...")
+                    time.sleep(2)
+                    print("Você se vira e vê o verdadeiro assassino na sala: alguém que você conhecia!")
+                    time.sleep(2)
+                    print("O assassino sorri, e você percebe que estava sendo manipulado o tempo todo!")
+                    time.sleep(2)
+                    print("\nParabéns, você encontrou a verdade... mas foi tarde demais!")
+                    morto()
+                elif escolha == "2":
+                    print("\nContinuar conversando com a voz não ajudou. A pressão das paredes começa a esmagar seu corpo!")
+                    arma()
+                else:
+                    print("\nVocê ficou paralisado. Não sabe se é um pesadelo ou realidade.")
+                    medonho()
+
+        elif escolha == "3":
+            print("\nVocê tenta sair da sala, mas a porta está trancada. E agora...? Você decide...")
+            arma()
+
+        else:
+            print("\nVocê ficou parado aí, como se estivesse tentando descobrir qual escolha fazer.")
+            bizzaro()
+
+
+    jogar()
+
+# Recusando
+elif decisao == "N" or decisao == "n":
+    print("Parabéns! Uma pessoa morreu pela sua preguiça hoje...")
+    time.sleep(3)
+    print("E acha que vai sair barato?")
+    print("Seu pagamento, vai ser o mesmo destino dele...")
+    time.sleep(8)
+    morto()
